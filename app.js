0c3c0ee414d21b7d24df81b1d474a3b9baacd70b
sarik <siddiquisarik@gmail.com>
2023-02-25 01:42:47 +0530

    trid ll

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ var app = express();
 
 app.use('/static', express.static('public'));
 
+app.get('/', (req, res) => {
+    res.send('Hello World, from express');
+});
+
+app.post('/ax', (req, res) => {
+    res.status(204).send('Hello World, from express');
+});
+
 app.listen(3001);
